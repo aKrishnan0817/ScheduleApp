daa13ed06c0e717d16d70a780ac3de71a48fb0ea
aKrishnan0817 <atul.krishnan07@gmail.com>
2020-02-25 11:41:26 -0500

    Create Dictionary for SCH

---
+++ dictionary.py
@@ -0,0 +1,314 @@
+posts = {
+    "Monday" :[
+        {
+            "class" : "Science",
+            "room" : "Lab",
+            "teacher" : "Taylor",
+            "time" :"8:30 AM"
+        },
+        {
+            "class" : "Free",
+            "room" : "Library",
+            "teacher" : "Haydee",
+            "time" :"9:15 AM"
+        },
+        {
+            "class" : "Latin",
+            "room" : "1A",
+            "teacher" : "Bruce",
+            "time" :"10:00 AM"
+        },
+        {
+            "class" : "Snack",
+            "room" : "2B",
+            "teacher" : "John",
+            "time" :"10:45 AM"
+        },
+        {
+            "class" : "Data Science",
+            "room" : "2F",
+            "teacher" : "Tawio",
+            "time" :"10:55 AM"
+        },
+        {
+            "class" : "Free",
+            "room" : "Library",
+            "teacher" : "Haydee",
+            "time" :"11:40 AM"
+        },
+        {
+            "class" : "Theater",
+            "room" : "Dance Room",
+            "teacher" : "Jessie",
+            "time" :"12:25 AM"
+        },
+        {
+            "class" : "History",
+            "room" : "R2",
+            "teacher" : "Sofia",
+            "time" :"1:10 PM"
+        },
+        {
+            "class" : "Math",
+            "room" : "2E",
+            "teacher" : "Shiela",
+            "time" :"1:55 PM"
+        },
+        {
+            "class" : "English",
+            "room" : "1A",
+            "teacher" : "Zach",
+            "time" :"2:40 PM"
+        }
+    ],
+    "Tuesday" :[
+        {
+            "class" : "Data Science",
+            "room" : "2F",
+            "teacher" : "Tawio",
+            "time" :"8:30 AM"
+        },
+        {
+            "class" : "Creative Writing",
+            "room" : "R2",
+            "teacher" : "Evan",
+            "time" :"9:15 AM"
+        },
+        {
+            "class" : "Medival History",
+            "room" : "R2",
+            "teacher" : "Sofia",
+            "time" :"10:00 AM"
+        },
+        {
+            "class" : "Snack",
+            "room" : "2B",
+            "teacher" : "John",
+            "time" :"10:45 AM"
+        },
+        {
+            "class":"Back End-Development",
+            "room":"2F",
+            "teacher":"Julio",
+            "time" :"10:55 AM"
+        },
+        {
+            "class":"Greek",
+            "room":"2C",
+            "teacher":"Maria",
+            "time" :"11:40 AM"
+        },
+        {
+            "class":"Lunch",
+            "room":"any where",
+            "teacher":"no one",
+            "time" :"12:25 AM"
+        },
+        {
+            "class":"Latin",
+            "room":"1A",
+            "teacher":"Bruce",
+            "time" :"1:10 PM"
+        },
+        {
+            "class":"Free",
+            "room":"Library",
+            "teacher":"Haydee",
+            "time" :"1:55 PM"
+        },
+        {
+            "class":"English",
+            "room":"1A",
+            "teacher":"Zach",
+            "time" :"2:40 PM"
+        }
+    ],
+    "Wenesday" :[
+        {
+            "class": "Free",
+            "room": "Library",
+            "teacher" : "Haydee",
+            "time" :"8:30 AM"
+        },
+        {
+            "class": "English",
+            "room": "1A",
+            "teacher":"Zach",
+            "time" :"9:15 AM"
+        },
+        {
+            "class": "Art",
+            "room": "Art Room",
+            "teacher": "Maricio",
+            "time" :"10:00 AM"
+        },
+        {
+            "class": "Snack",
+            "room": "Any Where",
+            "teacher" : "No one",
+            "time" :"10:45 AM"
+        },
+        {
+            "class": "Greek",
+            "room": "2C",
+            "teacher":"Maria",
+            "time" :"10:55 AM"
+        },
+        {
+            "class": "Math",
+            "room": "2E",
+            "teacher":"Shiela",
+            "time" :"11:40 AM"
+        },
+        {
+            "class": "Lunch",
+            "room": "Right Lobby",
+            "teacher" : "No one",
+            "time" :"12:25 AM"
+        },
+        {
+            "class": "VGP",
+            "room": "1A",
+            "teacher" : "Eddie",
+            "time" :"1:10 PM"
+        },
+        {
+            "class": "Free",
+            "room": "Library",
+            "teacher" : "Haydee",
+            "time" :"1:55 PM"
+        },
+        {
+            "class": "Science",
+            "room": "Lab",
+            "teacher" : "Taylor",
+            "time" :"2:40 PM"
+        }
+    ],
+    "Thursday" :[
+        {
+            "class" : "History",
+            "room" : "R2",
+            "teacher" : "Sofia",
+            "time" :"8:30 AM",
+            "css" : "history"
+        },
+        {
+            "class" : "Back End Development",
+            "room" : "2F",
+            "teacher" : "Julio",
+            "time":"9:15 AM",
+            "css":"backEnd"
+        },
+        {
+            "class" : "Latin",
+            "room" : "1A",
+            "teacher" : "Bruce",
+            "time" :"10:00 AM"
+        },
+        {
+            "class" : "Snack",
+            "room" : "Any Where",
+            "teacher" : "No one",
+            "time" :"10:45 AM"
+        },
+        {
+            "class" : "english",
+            "room" : "1A",
+            "teacher" : "Zach",
+            "time" :"10:55 AM"
+        },
+        {
+            "class" : "Data Science",
+            "room" : "Library",
+            "teacher" : "Taiwo",
+            "time" :"11:40 AM"
+        },
+        {
+            "class" : "Art",
+            "room" : "Art Room",
+            "teacher" : "Maricio",
+            "time" :"12:25 AM"
+        },
+        {
+            "class" : "Greek",
+            "room" : "2C",
+            "teacher" : "Maria",
+            "time" :"1:10 PM"
+        },
+        {
+            "class" : "Math",
+            "room" : "2E",
+            "teacher" : "Shiela",
+            "time" :"1:55 PM"
+        },
+        {
+            "class" : "Science",
+            "room" : "Lab",
+            "teacher" : "Taylor",
+            "time" :"2:40 PM"
+        }
+    ],
+    "Friday" :[
+        {
+            "class" : "Math",
+            "room" : "2E",
+            "teacher" : "Shiela",
+            "time" :"8:30 AM"
+        },
+        {
+            "class" : "Greek",
+            "room" : "2C",
+            "teacher" : "Maria",
+            "time" :"9:15 AM"
+        },
+        {
+            "class" : "Back End Development",
+            "room" : "2F",
+            "teacher" : "Julio",
+            "time" :"10:00 AM"
+        },
+        {
+            "class" : "Snack",
+            "room" : "Any Where",
+            "teacher" : "No one",
+            "time" :"10:45 AM"
+        },
+        {
+            "class" : "History",
+            "room" : "R2",
+            "teacher" : "Sofia",
+            "time" :"10:55 AM"
+        },
+        {
+            "class" : "Free",
+            "room" : "Library",
+            "teacher" : "Haydee",
+            "time" :"11:40 AM"
+        },
+        {
+            "class" : "No class ",
+            "room" : "Any Where",
+            "teacher" : "No teacher",
+            "time" :"12:25 AM"
+        },
+        {
+            "class" : "Latin",
+            "room" : "2E",
+            "teacher" : "Bruce",
+            "time" :"1:10 PM"
+        },
+        {
+            "class" : "Science",
+            "room" : "R2",
+            "teacher" : "Taylor",
+            "time" :"1:55 PM"
+        },
+        {
+            "class" : "Free",
+            "room" : "Any Where",
+            "teacher" : "No one",
+            "time" :"2:40 PM"
+        }
+    ]
+}
